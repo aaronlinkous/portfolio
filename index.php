e86b810e6f2897e76de64cb72dc79a32cb74865a
Aaron Linkous <aaron@bellmobi.com>
2013-10-22 11:43:18 -0700

    basic index. renamed misc -> js.js

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Aaron Linkous, Sr. Developer</title>
+		<link rel="stylesheet" type="text/css" href="css/css.css" media="screen" />
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+		<script src="js/js.js"></script>
+	</head>
+	<body>
+		
+	</body>
+</html>
