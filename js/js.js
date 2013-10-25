8aca43848ab2589834fcf0076a7b4a863f95304f
Aaron Linkous <aaron@bellmobi.com>
2013-10-25 15:06:23 -0700

    streamlined button styles and added Title rotation

--- js/js.js
+++ js/js.js
@@ -1,4 +1,6 @@
-var header_height,marquee_height,height,width;
+var header_height,marquee_height,height,width,time = 250;
+
+var titles = ["Sr. Developer", "UI & UX Designer", "Analogy Extraordinaire", "Project Manager"];
 
 function getSize() {
 	height = $(window).outerHeight();
@@ -7,9 +9,21 @@ function getSize() {
 	header_height = $("header").outerHeight();
 	marquee_height = $("#marquee").outerHeight();
 }
+var i = 0;
+var l = titles.length-1;
+
+function loop_titles() {
+	$(".title").fadeOut(time,function(){
+		$(this).text(titles[i]).fadeIn(time);
+	});
+	
+	i = i == l ? 0 : i + 1;
+	setTimeout(loop_titles, 2000+(time*2));
+}
 
 $(document).ready(function(){
 	getSize();
+	setTimeout(loop_titles, 2000);
 
 	$(window).resize(function(){
 		getSize();
