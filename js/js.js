57aadb6438c6ec77ba1af63d2601b57fb9c0fc11
Aaron Linkous <aaron@bellmobi.com>
2013-10-28 12:55:11 -0700

    cleanup

--- js/js.js
+++ js/js.js
@@ -1,5 +1,4 @@
 var header_height,marquee_height,height,width,time = 250;
-
 var titles = ["Sr. Developer", "UI & UX Designer", "Analogy Extraordinaire", "Project Manager"];
 
 function getSize() {
@@ -9,14 +8,14 @@ function getSize() {
 	header_height = $("header").outerHeight();
 	marquee_height = $("#marquee").outerHeight();
 }
+
 var i = 0;
 var l = titles.length-1;
-
 function loop_titles() {
 	$(".title").fadeOut(time,function(){
 		$(this).text(titles[i]).fadeIn(time);
 	});
-	
+
 	i = i == l ? 0 : i + 1;
 	setTimeout(loop_titles, 2000+(time*2));
 }
